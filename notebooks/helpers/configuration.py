304053d597cc63e16483a01031f1707129ea7726
Salman Lashkarara <salmanlashkarara@gmail.com>
2022-12-05 20:43:28 +0100

    update sink path

--- notebooks/helpers/configuration.py
+++ notebooks/helpers/configuration.py
@@ -3,6 +3,6 @@
 
 # COMMAND ----------
 
-RAW_FOLDER_PATH = 'dbfs:/mnt/formuleinsstorage/rawdata/unziped/'
+RAW_FOLDER_PATH = 'dbfs:/mnt/formuleinsstorage/rawdata/'
 PROCESSED_FOLDER_PATH = 'dbfs:/mnt/formuleinsstorage/processeddata'
 MESSAGE_ON_NOTEBOOK_SUCCESSY = 'Success'

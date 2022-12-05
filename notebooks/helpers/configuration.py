72b8c5735e4d2b5f25fdd9cfe63a901130aa554c
Salman Lashkarara <salmanlashkarara@gmail.com>
2022-12-05 11:00:28 +0100

    Configuration contains the path to raw and processed container and also the message when notebook is successfully executed

--- notebooks/helpers/configuration.py
+++ notebooks/helpers/configuration.py
@@ -3,8 +3,9 @@ display(dbutils.fs.mounts())
 
 # COMMAND ----------
 
-raw_folder_path = 'dbfs:/mnt/formuleinsstorage/rawdata'
+raw_folder_path = 'dbfs:/mnt/formuleinsstorage/rawdata/unziped/'
 processed_folder_path = 'dbfs:/mnt/formuleinsstorage/processeddata'
+MESSAGE_TO_WHEN_COMPLETING_NOTEBOOK_SUCCESSFULLY = 'Success'
 
 # COMMAND ----------
 

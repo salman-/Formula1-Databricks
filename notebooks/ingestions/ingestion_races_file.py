6d792b00b494443b14d24d95803181d212596442
Salman Lashkarara <salmanlashkarara@gmail.com>
2022-12-05 12:57:38 +0100

    none existing column cannot be renamed

--- notebooks/ingestions/ingestion_races_file.py
+++ notebooks/ingestions/ingestion_races_file.py
@@ -12,13 +12,7 @@ circuits_df = read_csv_file(f"{RAW_FOLDER_PATH}/{FILE_NAME}.csv")
 circuits_df = make_all_columns_snake_case(circuits_df)
 circuits_df = add_time_stamp_to_dataframe(circuits_df, FILE_NAME)
 
-circuits_df = (
-    circuits_df.withColumnRenamed("name", f"{FILE_NAME}_name")
-    .withColumnRenamed("lat", "latitude")
-    .withColumnRenamed("lng", "longitude")
-    .withColumnRenamed("alt", "altitude")
-    .drop("url")
-)
+circuits_df = circuits_df.withColumnRenamed("name", f"{FILE_NAME}_name").drop("url")
 
 save_dataframe_as_parquet(circuits_df, f'{PROCESSED_FOLDER_PATH}/{FILE_NAME}')
 dbutils.notebook.exit(MESSAGE_ON_NOTEBOOK_SUCCESSY)

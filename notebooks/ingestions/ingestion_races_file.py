55749d33f5b78755f7a87cf3214c4713058b34a5
Salman Lashkarara <salmanlashkarara@gmail.com>
2022-12-05 16:27:36 +0100

    rename dataframe

--- notebooks/ingestions/ingestion_races_file.py
+++ notebooks/ingestions/ingestion_races_file.py
@@ -9,10 +9,10 @@
 
 FILE_NAME = "races"
 races_df = read_csv_file(f"{RAW_FOLDER_PATH}/{FILE_NAME}.csv")
-races_df = make_all_columns_snake_case(circuits_df)
-races_df = add_time_stamp_to_dataframe(circuits_df, FILE_NAME)
+races_df = make_all_columns_snake_case(races_df)
+races_df = add_time_stamp_to_dataframe(races_df, FILE_NAME)
 
-races_df = circuits_df.withColumnRenamed("name", f"{FILE_NAME}_name").drop("url")
+races_df = races_df.withColumnRenamed("name", f"{FILE_NAME}_name").drop("url")
 
 save_dataframe_as_parquet(races_df, f"{PROCESSED_FOLDER_PATH}/{FILE_NAME}")
 dbutils.notebook.exit(MESSAGE_ON_NOTEBOOK_SUCCESSY)

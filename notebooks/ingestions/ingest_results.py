47343f3dcecce6edaefd14bc2addda25a754b491
Salman Lashkarara <salmanlashkarara@gmail.com>
2022-12-05 15:47:02 +0100

    ingest results file

---
+++ notebooks/ingestions/ingest_results.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %run "./../helpers/configuration"
+
+# COMMAND ----------
+
+# MAGIC %run "./../helpers/functions"
+
+# COMMAND ----------
+
+FILE_NAME = "results"
+results_df = read_csv_file(f"{RAW_FOLDER_PATH}/{FILE_NAME}.csv")
+results_df = make_all_columns_snake_case(results_df)
+results_df = add_time_stamp_to_dataframe(results_df, FILE_NAME)
+results_df = results_df.drop("status_id")
+
+save_and_partition_by_dataframe_as_parquet(
+    results_df, "race_id", f"{PROCESSED_FOLDER_PATH}/{FILE_NAME}"
+)
+dbutils.notebook.exit(MESSAGE_ON_NOTEBOOK_SUCCESSY)

9b3584fbd8b87eda821d4116da6587cc8aa8bafb
Salman Lashkarara <salmanlashkarara@gmail.com>
2022-12-05 16:18:56 +0100

    ingest all resources

---
+++ notebooks/all_ingestions.py
@@ -0,0 +1,38 @@
+# Databricks notebook source
+result = dbutils.notebook.run('./ingestions/ingest_circuite',0)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run('./ingestions/ingest_constructors',0)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run('./ingestions/ingest_driver',0)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run('./ingestions/ingest_pitstops',0)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run('./ingestions/ingest_results',0)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run('./ingestions/ingestion_races',0)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run('./ingestions/ingest_lap_times',0)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run('./ingestions/ingest_qualifying',0)
+result

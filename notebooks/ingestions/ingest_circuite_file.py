5d9d0b3d31ce468d5d6e3534ea9bd2716499277b
Salman Lashkarara <salmanlashkarara@gmail.com>
2022-12-05 12:52:24 +0100

    ingest circiut.csv

--- notebooks/ingestions/ingest_circuite_file.py
+++ notebooks/ingestions/ingest_circuite_file.py
@@ -1,8 +1,14 @@
 # Databricks notebook source
-dbutils.notebook.run("./../helpers/configuration", 0)
-dbutils.notebook.run("./../helpers/functions", 0)
+# MAGIC %run "./../helpers/configuration"
 
-circuits_df = read_csv_file(f"{raw_folder_path}/circuits.csv")
+# COMMAND ----------
+
+# MAGIC %run "./../helpers/functions"
+
+# COMMAND ----------
+
+FILE_NAME = "circuits"
+circuits_df = read_csv_file(f"{RAW_FOLDER_PATH}/{FILE_NAME}.csv")
 circuits_df = make_all_columns_snake_case(circuits_df)
 circuits_df = add_time_stamp_to_dataframe(circuits_df, "circuits")
 
@@ -14,5 +20,5 @@ circuits_df = (
     .drop("url")
 )
 
-save_dataframe_as_parquet(circuits_df, "circiutes")
+save_dataframe_as_parquet(circuits_df, f'{PROCESSED_FOLDER_PATH}/{FILE_NAME}')
 dbutils.notebook.exit(MESSAGE_TO_WHEN_COMPLETING_NOTEBOOK_SUCCESSFULLY)

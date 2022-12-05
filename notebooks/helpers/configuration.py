388719bfc33ea9bf45d9e452fa1d1fbbcd6efed0
Salman Lashkarara <salmanlashkarara@gmail.com>
2022-12-05 11:18:37 +0100

    remove unused magic commands

--- notebooks/helpers/configuration.py
+++ notebooks/helpers/configuration.py
@@ -1,18 +1,8 @@
 # Databricks notebook source
-display(dbutils.fs.mounts())
+#display(dbutils.fs.mounts())
 
 # COMMAND ----------
 
 raw_folder_path = 'dbfs:/mnt/formuleinsstorage/rawdata/unziped/'
 processed_folder_path = 'dbfs:/mnt/formuleinsstorage/processeddata'
 MESSAGE_TO_WHEN_COMPLETING_NOTEBOOK_SUCCESSFULLY = 'Success'
-
-# COMMAND ----------
-
-# MAGIC %fs
-# MAGIC ls /mnt/formuleinsstorage/rawdata
-
-# COMMAND ----------
-
-# MAGIC %fs
-# MAGIC ls /mnt/formuleinsstorage/processeddata

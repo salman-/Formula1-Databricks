1980f7028e7708a7000c40ce81fd4e31c7e1e7a5
Salman Lashkarara <salmanlashkarara@gmail.com>
2022-12-05 14:38:37 +0100

    rename variable

--- notebooks/ingestions/ingestion_races_file.py
+++ notebooks/ingestions/ingestion_races_file.py
@@ -8,11 +8,11 @@
 # COMMAND ----------
 
 FILE_NAME = "races"
-circuits_df = read_csv_file(f"{RAW_FOLDER_PATH}/{FILE_NAME}.csv")
-circuits_df = make_all_columns_snake_case(circuits_df)
-circuits_df = add_time_stamp_to_dataframe(circuits_df, FILE_NAME)
+races_df = read_csv_file(f"{RAW_FOLDER_PATH}/{FILE_NAME}.csv")
+races_df = make_all_columns_snake_case(circuits_df)
+races_df = add_time_stamp_to_dataframe(circuits_df, FILE_NAME)
 
-circuits_df = circuits_df.withColumnRenamed("name", f"{FILE_NAME}_name").drop("url")
+races_df = circuits_df.withColumnRenamed("name", f"{FILE_NAME}_name").drop("url")
 
 save_dataframe_as_parquet(circuits_df, f'{PROCESSED_FOLDER_PATH}/{FILE_NAME}')
 dbutils.notebook.exit(MESSAGE_ON_NOTEBOOK_SUCCESSY)

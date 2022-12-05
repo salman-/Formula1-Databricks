7528d9c6c2942c6a2663179daa00692b3fabf25b
Salman Lashkarara <salmanlashkarara@gmail.com>
2022-12-05 14:45:50 +0100

    update prefix of column

--- notebooks/ingestions/ingest_circuite_file.py
+++ notebooks/ingestions/ingest_circuite_file.py
@@ -10,7 +10,7 @@
 FILE_NAME = "circuits"
 circuits_df = read_csv_file(f"{RAW_FOLDER_PATH}/{FILE_NAME}.csv")
 circuits_df = make_all_columns_snake_case(circuits_df)
-circuits_df = add_time_stamp_to_dataframe(circuits_df, "circuits")
+circuits_df = add_time_stamp_to_dataframe(circuits_df, FILE_NAME)
 
 circuits_df = (
     circuits_df.withColumnRenamed("name", "circuit_name")
@@ -21,4 +21,4 @@ circuits_df = (
 )
 
 save_dataframe_as_parquet(circuits_df, f'{PROCESSED_FOLDER_PATH}/{FILE_NAME}')
-dbutils.notebook.exit(MESSAGE_TO_WHEN_COMPLETING_NOTEBOOK_SUCCESSFULLY)
+dbutils.notebook.exit(MESSAGE_ON_NOTEBOOK_SUCCESSY)

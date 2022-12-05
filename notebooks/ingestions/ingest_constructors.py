06e7cfaf9286d33c8af814504fc35eab4f954111
Salman Lashkarara <salmanlashkarara@gmail.com>
2022-12-05 14:42:35 +0100

    change prefix of timestamp column

--- notebooks/ingestions/ingest_constructors.py
+++ notebooks/ingestions/ingest_constructors.py
@@ -10,7 +10,7 @@
 FILE_NAME = "constructors"
 constructors_df = read_csv_file(f"{RAW_FOLDER_PATH}/{FILE_NAME}.csv")
 constructors_df = make_all_columns_snake_case(constructors_df)
-constructors_df = add_time_stamp_to_dataframe(constructors_df, "circuits")
+constructors_df = add_time_stamp_to_dataframe(constructors_df, FILE_NAME)
 
 
 save_dataframe_as_parquet(constructors_df, f'{PROCESSED_FOLDER_PATH}/{FILE_NAME}')
